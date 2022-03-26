ce8092bcb247a6d96c43dbc74142450020110ea1
bbangji <99783573+bbangji@users.noreply.github.com>
2022-03-26 15:10:45 +0900

    this is commit!

---
+++ Solution.java
@@ -0,0 +1,12 @@
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		int[] nums = {3,1,2,3,4,2};
+		boolean[] check = new boolean[nums.length];
+		int n = nums.length/2;
+		
+		
+		
+	}
+}
